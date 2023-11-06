cfac31896bead47987e83ea58dbc00ac87bcc389
Mateo Castano <82683246+MateCastano@users.noreply.github.com>
2023-11-06 19:56:00 -0300

    Update 06/11

--- proyectoRecitales/public/contacto.php
+++ proyectoRecitales/public/contacto.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../styles/normalize.css">
     <link rel="stylesheet" href="../styles/style.css">
+
     <title>TicketNow</title>
 
 </head>
+
+<?php include("../public/conection.php"); 
+
+    $resultado = mysqli_query($conection, "SELECT * FROM recital");
+    
+?>
+
 <body>
-<header class="header">
+
+    <header class="header">
             <a class="header-logo" href="../public/index.php">
                 <img href="../public/index.php" src="../images/logo.png" alt="inicio">
                 </a>
@@ -20,36 +29,46 @@
                     <li><a href="../public/index.php">Inicio</a></li>
                     <li><a href="../public/nosotros.php">Nosotros</a></li>
                     <li><a href="../public/contacto.php">Contacto</a></li>
-                    <li><a href="../public/login.php">Mi cuenta</a></li>
+                    <li><a href="../public/account-verification.php">Mi cuenta</a></li>
+                    <?php 
+                        session_start();
+
+                        if(isset($_SESSION['membresia']) && $_SESSION['membresia'] === 'Administrador')
+                        {
+                        echo '<li><a href="../private/admin.php">Panel de Admin</a></li>';
+                            
+                        }
+                        
+                    ?>
                 </ul>
             </nav>        
     </header>
+    <div class="contacto form">
     <main class="main">
-        <form class="formulario" action="../public/register-verification.php" method="post">
-            <h2>Ingrese sus datos</h2>
-            <br>
-            <h3>Nombre:</h3>
-                <input type="text" name="name" required class="text"/>
-            <h3>Apellido:</h3>
-                <input type="text" name="surname" required class="text"/>
-            <h3>Email:</h3>
-                <input type="email" name="email" required class="text"/>
+        <form class="formularioContacto" action="../public/enviarContacto.php" method="post">
+        <h2 class="contactH2">Envianos tus consultas mediante el siguiente formulario</h2>
+            <br><h3>Nombre:</h3>
+                <input type="text" name="nombre" required  class="textContacto"/>
+            <h3>Apellido:</h3>  
+                <input type="text" name="apellido" required  class="textContacto"/>
             <h3>Nombre de usuario:</h3>
-                <input type="text" name="username" maxlength="30" required class="text"/>
-            <h3>Contraseña:</h3>
-                <input type="password" name="password" maxlength="30" required class="text"/>
-            <input class="buttom" type="submit" value="Registrarse"/>
-                <p>¿Ya tienes cuenta? Incia sesion aqui</p>
-            <a href="../public/login.php">Inciar sesion</a>	    
+                <input type="text" name="username" required  class="textContacto"/>
+            <h3>Email:</h3>
+                <input type="email" name="email" required  class="textContacto"/>
+            <br>
+            <br>
+            <textarea name="user_message" placeholder="Escriba aqui su mensaje" required="true" required></textarea>
+            <br>
+            <input class="buttom" type="submit" value="Enviar"/>	
         </form>
-    </main>
+    </main> 
     <footer class="footer">
         <div class="footer-links">
         <nav class="navbar">
                 <ul>
                     <li><a href="../public/index.php">Inicio</a></li>
                     <li><a href="#">Nosotros</a></li>
-                    <li><a href="#">Contacto</a></li>
+                    <li><a href="../public/contacto.php">Contacto</a></li>
                     <li><a href="../public/account-verification.php">Mi cuenta</a></li>
                     <?php
 
