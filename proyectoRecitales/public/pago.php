bf1869c0ba7596563baed17eccbc07756fd3edbb
Mateo Castano <82683246+MateCastano@users.noreply.github.com>
2023-11-14 11:39:58 -0300

    Update pago.php

--- proyectoRecitales/public/pago.php
+++ proyectoRecitales/public/pago.php
@@ -165,7 +165,7 @@
         {
             echo '<div class="compra-realizada">';
             echo '<h3>Incie sesion para poder realizar esta compra.</h3>';
-            echo '<a href="../public/login.php"><button class="buttom">Log-In</button></a>';
+            echo '<a href="../public/login.php"><button class="buttom">Iniciar sesion</button></a>';
             echo '</div>';
         }
         ?>
